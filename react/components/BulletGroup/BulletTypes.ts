0e2893e5f907019ab8c4672589b3fa48a7f6371d
jaun0819 <aguileraaraquea@gmail.com>
2022-12-05 09:14:58 -0500

    feat:custom-list-context

---
+++ react/components/BulletGroup/BulletTypes.ts
@@ -0,0 +1,12 @@
+export type BulletsSchema = Array<{
+    image: string
+    titleBullet: string
+    link?: LinkProps
+}>
+export interface LinkProps {
+    url: string
+    attributeNofollow?: boolean
+    attributeTitle?: string
+    openNewTab?: boolean
+    newTab?: boolean
+}
---
+++ react/components/BulletGroup/index.tsx
@@ -0,0 +1,40 @@
+import React, { PropsWithChildren } from 'react'
+import { useListContext, ListContextProvider } from 'vtex.list-context'
+import { BulletsSchema } from './BulletTypes'
+import { useDevice } from 'vtex.device-detector'
+import { getBulletsAsTSXList } from './modules/bulletsAsList'
+
+export interface BulletGroupProps {
+    bullets: BulletsSchema
+}
+
+
+const BulletGroup = ({
+    bullets,
+    children
+}: PropsWithChildren<BulletGroupProps>) => {
+    const { isMobile } = useDevice();
+    const { list } = useListContext() || []
+
+
+    console.log("Bullets", bullets)
+
+    const bulletsGroup = getBulletsAsTSXList(bullets);
+    const newListContextValue = list.concat(bulletsGroup)
+
+    if (false) {
+        console.log(children, list)
+    }
+    return (
+        <ListContextProvider list={newListContextValue}>
+            {
+                isMobile
+                    ?
+                    <div>{bulletsGroup}</div>
+                    :
+                    children
+            }
+        </ListContextProvider>
+    )
+}
+export default BulletGroup
